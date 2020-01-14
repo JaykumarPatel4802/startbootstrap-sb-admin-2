04b11a1d843a1b0ef8423a9ae57087794252457c
Jaykumar Patel <jaykumar.patel4802@gmail.com>
2020-01-14 23:03:08 +0000

    Make changes to dashboard and simluation pages

--- flask_app.py
+++ flask_app.py
@@ -2,6 +2,11 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def main():
+def index():
     # found in ../templates/
     return render_template("index.html")
+
+@app.route('/simulation', methods=['GET'])
+def simulation():
+    # found in ../templates/
+    return render_template("simulation.html")
