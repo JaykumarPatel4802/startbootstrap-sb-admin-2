d29b01d2d32638bd3b90eab7c3b7a1f1f948c3fc
Jaykumar Patel <jaykumar.patel4802@gmail.com>
2020-02-13 05:48:46 +0000

    Add the simulation and connect it to 'simulation_data' table

--- flask_app.py
+++ flask_app.py
@@ -47,9 +47,43 @@ def dashboard():
 
     return render_template("dashboard.html", full_data=data, last_data=last_data)
 
-@app.route('/simulation', methods=['GET'])
+@app.route('/simulation', methods=['GET','POST'])
 def simulation():
     # found in ../templates/
+    if(request.method == "POST"):
+        data = str(request.get_data())
+
+        temp = data[data.find('totalStudents') + 15:]
+        totalStudents = temp[:temp.find(',')]
+
+        temp = data[data.find('studentsPerRoom') + 17:]
+        studentsPerRoom = temp[:temp.find(',')]
+
+        temp = data[data.find('numberOfRooms') + 15:]
+        numberOfRooms = temp[:temp.find(',')]
+
+        temp = data[data.find('averageTime') + 13:]
+        averageTime = temp[:temp.find(',')]
+
+        temp = data[data.find('maxTime') + 9:]
+        maxTime = temp[:temp.find(',')]
+
+        temp = data[data.find('minTime') + 9:]
+        minTime = temp[:temp.find('}')]
+
+        mydb = mysql.connector.connect(
+        host = "JaykumarPatel4802.mysql.pythonanywhere-services.com",
+        user = "JaykumarPatel480",
+        passwd = "MySQLDataBase",
+        database = "JaykumarPatel480$Simulation",
+        )
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO simulation_data (totalStudents, studentsPerRoom, numberOfRooms, averageTime, maxTime, minTime) VALUES (%s, %s, %s, %s, %s, %s)"
+        mycursor.execute(sql, (totalStudents, studentsPerRoom, numberOfRooms, averageTime, maxTime, minTime))
+        mydb.commit()
+
+        return str(totalStudents) + " " + str(studentsPerRoom) + " " + str(numberOfRooms) + " " + str(averageTime) + " " + str(maxTime) + " " + str(minTime)
+
     return render_template("simulation.html")
 
 
